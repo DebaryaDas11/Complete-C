0813769f375ca2d70347f6c671fa9c34bfb2714a
DebaryaDas11 <debad4954@gmail.com>
2026-04-24 09:49:29 +0530

    Adding

---
+++ DAY1-4.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    int a , b;
+    printf("Enter Length and Breadth(cm):");
+    scanf("%d %d", &a,&b);
+    printf("Area Of the rectangle is : %d cm", a * b);
+    return 0;
+}
